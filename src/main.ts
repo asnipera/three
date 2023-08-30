48fb687689aed8241bc6b1ca9d5aaa3ff4a4e36d
李彦 <liyan@neotrident.com>
2023-08-30 20:08:29 +0800

    🖥️wip: cif

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { Tween, Easing, update } from "three/examples/jsm/libs/tween.module.js";
 // gui
 import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
-
 // 创建场景
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("#ffffff");
@@ -26,13 +25,16 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(-4, 1.5, 1.2);
+camera.position.set(-15, 0, 0);
 
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// 坐标系
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 /**
  * Light
  */
@@ -57,99 +59,101 @@ const hemisphereLight = new THREE.HemisphereLight(
   1
 );
 scene.add(hemisphereLight);
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
 
+// // 创建一个环境光
+// var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
+
+// // 创建一个定向光源
+// var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// directionalLight.position.set(1, 1, 1); // 设置光源的方向
+// scene.add(directionalLight);
+
+const cif = `# generated using pymatgen
+data_Ac2CuSi
+_symmetry_space_group_name_H-M   'P 1'
+_cell_length_a   7.65983084
+_cell_length_b   7.65983084
+_cell_length_c   7.65983084
+_cell_angle_alpha   90.00000000
+_cell_angle_beta   90.00000000
+_cell_angle_gamma   90.00000000
+_symmetry_Int_Tables_number   1
+_chemical_formula_structural   Ac2CuSi
+_chemical_formula_sum   'Ac8 Cu4 Si4'
+_cell_volume   449.42531996
+_cell_formula_units_Z   4
+loop_
+ _symmetry_equiv_pos_site_id
+ _symmetry_equiv_pos_as_xyz
+  1  'x, y, z'
+loop_
+ _atom_site_type_symbol
+ _atom_site_label
+ _atom_site_symmetry_multiplicity
+ _atom_site_fract_x
+ _atom_site_fract_y
+ _atom_site_fract_z
+ _atom_site_occupancy
+  Ac  Ac0  1  0.75000000  0.75000000  0.25000000  1
+  Ac  Ac1  1  0.25000000  0.75000000  0.25000000  1
+  Ac  Ac2  1  0.75000000  0.25000000  0.75000000  1
+  Ac  Ac3  1  0.25000000  0.25000000  0.75000000  1
+  Ac  Ac4  1  0.25000000  0.75000000  0.75000000  1
+  Ac  Ac5  1  0.75000000  0.75000000  0.75000000  1
+  Ac  Ac6  1  0.25000000  0.25000000  0.25000000  1
+  Ac  Ac7  1  0.75000000  0.25000000  0.25000000  1
+  Cu  Cu8  1  0.00000000  0.50000000  0.00000000  1
+  Cu  Cu9  1  0.00000000  0.00000000  0.50000000  1
+  Cu  Cu10  1  0.50000000  0.50000000  0.50000000  1
+  Cu  Cu11  1  0.50000000  0.00000000  0.00000000  1
+  Si  Si12  1  0.00000000  0.00000000  0.00000000  1
+  Si  Si13  1  0.00000000  0.50000000  0.50000000  1
+  Si  Si14  1  0.50000000  0.00000000  0.50000000  1
+  Si  Si15  1  0.50000000  0.50000000  0.00000000  1
+`;
+const mol = ChemDoodle.readCIF(cif);
+const model = new THREE.Group();
+const molecule = mol.molecule;
+const atoms = molecule.atoms;
+const atomCount = atoms.length;
 const gltfLoader = new GLTFLoader();
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("/draco/");
-gltfLoader.setDRACOLoader(dracoLoader);
-let left_door: THREE.Group;
-gltfLoader.load("/box/16_door.gltf", (gltf) => {
-  gltf.scene.name = "left_door";
-  left_door = gltf.scene;
-  gltf.scene.scale.set(0.03, 0.03, 0.03);
-  gltf.scene.children[0].translateY(36);
-  gltf.scene.children[0].translateX(18);
-  gltf.scene.position.set(-0.5, 0, 1.08);
-  // scene.add(gltf.scene);
-});
-
-let squareHeight = 0;
-function createSquare(
-  row: number,
-  column: number,
-  square: THREE.Object3D<THREE.Event>,
-  fridge: THREE.Object3D<THREE.Event>,
-  rowSpan = 0.8,
-  prefixName = "square",
-  colSpan = 0.7
-) {
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < column; j++) {
-      const _square = square.clone();
-      _square.name = `${prefixName}_${i}_${j}`;
-      const box = new THREE.Box3().setFromObject(_square);
-      const size = box.getSize(new THREE.Vector3());
-      squareHeight = size.y;
-      _square.position.setY(_square.position.y + size.y * i + rowSpan * i);
-      _square.position.setZ(_square.position.z + size.z * j + colSpan * j);
-      _square.receiveShadow = true;
-      fridge.add(_square);
-    }
-  }
-}
-
-function createLattice(
-  row: number,
-  square: THREE.Object3D<THREE.Event>,
-  fridge: THREE.Object3D<THREE.Event>,
-  prefixName = "lattice",
-  span = 3.6
-) {
-  for (let i = 0; i < row; i++) {
-    const _square = square.clone();
-    _square.name = `${prefixName}_${i}`;
-    const currentY = _square.position.y;
-    _square.position.setY(currentY + squareHeight * i + span * (i + 1));
-    _square.receiveShadow = true;
-    fridge.add(_square);
-  }
-}
-
-let fridge_door: THREE.Group;
 gltfLoader.load(
-  "/box/33.gltf",
+  "/ball/ball.gltf",
   (gltf) => {
-    gltf.scene.scale.set(0.03, 0.03, 0.03);
-    const doorLight = new THREE.PointLight("#ffffff", 1, 7);
-    doorLight.position.set(0, 60, -10);
-    // helper
-    const doorLightHelper = new THREE.PointLightHelper(doorLight, 1);
-    doorLight.castShadow = true;
-    doorLight.shadow.camera.near = 1;
-    doorLight.shadow.camera.far = 20;
-    doorLight.shadow.mapSize.set(1024, 1024);
-    doorLight.shadow.radius = 5;
-    doorLight.shadow.bias = -0.0001;
-    doorLight.shadow.normalBias = 0.02;
-    doorLight.shadow.camera.updateProjectionMatrix();
-    gltf.scene.add(doorLight);
-    scene.add(doorLightHelper);
+    // gltf.scene.scale.set(30, 30, 30);
+    // const doorLight = new THREE.PointLight("#ffffff", 1, 7);
+    // doorLight.position.set(0, 60, -10);
+    // // helper
+    // const doorLightHelper = new THREE.PointLightHelper(doorLight, 1);
+    // doorLight.castShadow = true;
+    // doorLight.shadow.camera.near = 1;
+    // doorLight.shadow.camera.far = 20;
+    // doorLight.shadow.mapSize.set(1024, 1024);
+    // doorLight.shadow.radius = 5;
+    // doorLight.shadow.bias = -0.0001;
+    // doorLight.shadow.normalBias = 0.02;
+    // doorLight.shadow.camera.updateProjectionMatrix();
+    // gltf.scene.add(doorLight);
+    // scene.add(doorLightHelper);
 
-    gltf.scene.traverse((item) => {
-      if (item.name === "Cube") {
-        item.castShadow = true;
-        createSquare(5, 5, item, gltf.scene, 4.2);
-        item.visible = false;
-      }
-    });
+    // gltf.scene.traverse((item) => {
+    //   if (item.name === "Cube") {
+    //     item.castShadow = true;
+    //     createSquare(5, 5, item, gltf.scene, 4.2);
+    //     item.visible = false;
+    //   }
+    // });
 
-    gltf.scene.traverse((item) => {
-      if (item.name === "Box033_Chrome_0001") {
-        item.castShadow = true;
-        createLattice(5, item, gltf.scene);
-        item.visible = false;
-      }
-    });
+    // gltf.scene.traverse((item) => {
+    //   if (item.name === "Box033_Chrome_0001") {
+    //     item.castShadow = true;
+    //     createLattice(5, item, gltf.scene);
+    //     item.visible = false;
+    //   }
+    // });
     // const _scene2 = gltf.scene.clone();
     // _scene2.name = "right_door";
     // // _scene2.rotation.y = -Math.PI;
@@ -158,7 +162,24 @@ gltfLoader.load(
     // right_door = _scene2;
     // gltf.scene.add(doorLight);
     // scene.add(doorLightHelper);
-    scene.add(gltf.scene);
+    console.log(gltf);
+
+    for (let i = 0; i < atomCount; i++) {
+      const ball = gltf.scene.children[0].clone();
+      const atom = atoms[i];
+
+      const { x, y, z } = atom;
+      ball.position.set(x, y, z);
+      model.add(ball);
+    }
+
+    model.position.setX(-3);
+    model.position.setY(-3);
+    model.position.setZ(-3);
+
+    scene.add(model);
+
+    // scene.add(gltf.scene);
   },
   (progress) => {},
   (error) => {
@@ -167,120 +188,8 @@ gltfLoader.load(
   }
 );
 
-// gltfLoader.load(
-//   "/box/16_square_1.gltf",
-//   (gltf) => {
-//     gltf.scene.scale.set(0.03, 0.03, 0.03);
-//     gltf.scene.position.x = 0.4;
-//     // const _scene2 = gltf.scene.clone();
-//     // _scene2.name = "right_door";
-//     // // _scene2.rotation.y = -Math.PI;
-//     // _scene2.children[0].translateZ(1);
-//     // _scene2.position.set(0, 0, -2);
-//     // right_door = _scene2;
-//     scene.add(gltf.scene);
-//   },
-//   (progress) => {},
-//   (error) => {
-//     console.log("error");
-//     console.log(error);
-//   }
-// );
-
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-
-// 创建面板的几何体
-const geometry = new THREE.PlaneGeometry(2, 2);
-// 将面板的中心点移动到左侧边框
-geometry.translate(1, 0, 0);
-// 创建面板的材质（可以自定义颜色或纹理）
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  side: THREE.DoubleSide,
-});
-
-// 坐标系
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-// 创建面板的网格，并设置初始位置和旋转角度
-const panel = new THREE.Mesh(geometry, material);
-panel.position.set(-1, 0, 0);
-// panel.rotation.y = Math.PI / 2; // 初始旋转角度，这里使用弧度制
-panel.name = "door";
-// scene.add(panel);
-
-// // 动画循环
-// function animate() {
-//   // 调用旋转动画函数
-//   rotatePanel();
-//   // 渲染场景
-//   renderer.render(scene, camera);
-//   update();
-// }
-
-// // 启动动画循环
-// animate();
-
-// var tween = new Tween(panel.position)
-//   .to({ x: 100, y: 100, z: 100 }, 10000)
-//   .onUpdate(function (this: { lv: number }) {
-//     console.log(this);
-//   })
-//   .start();
-// tween.start();
-
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  update();
 }
 animate();
-
-// 角度换算成弧度
-function angleToRadian(angle: number) {
-  return (angle * Math.PI) / 180;
-}
-
-// 创建gui单击按钮
-const gui = new GUI();
-const folder = gui.addFolder("folder");
-folder
-  .add(
-    {
-      click: () =>
-        new Tween(left_door.rotation)
-          .to(
-            {
-              // 45度
-              y: angleToRadian(160),
-            },
-            1000
-          )
-          // 打开冰箱门
-          .easing(Easing.Linear.None)
-          .start(),
-    },
-    "click"
-  )
-  .name("打开左侧门");
-
-folder
-  .add(
-    {
-      click: () =>
-        new Tween(left_door.rotation)
-          .to(
-            {
-              y: angleToRadian(0),
-            },
-            1000
-          )
-          // 打开冰箱门
-          .easing(Easing.Linear.None)
-          .start(),
-    },
-    "click"
-  )
-  .name("打开右侧门");
