a4475ed211b59b2097f6a0055dbc6611a75918d1
李彦 <liyan@neotrident.com>
2023-08-31 14:53:05 +0800

    🧪test: 测试SDH晶体结构

--- src/main.ts
+++ src/main.ts
@@ -72,138 +72,159 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // directionalLight.position.set(1, 1, 1); // 设置光源的方向
 // scene.add(directionalLight);
 
-const cif = `# generated using pymatgen
-data_Ac
-_symmetry_space_group_name_H-M   'P 1'
-_cell_length_a   4.04604077
-_cell_length_b   4.04604077
-_cell_length_c   13.01703648
-_cell_angle_alpha   90.00000000
-_cell_angle_beta   90.00000000
-_cell_angle_gamma   120.00000000
-_symmetry_Int_Tables_number   1
-_chemical_formula_structural   Ac
-_chemical_formula_sum   Ac4
-_cell_volume   184.54541612
-_cell_formula_units_Z   4
-loop_
- _symmetry_equiv_pos_site_id
- _symmetry_equiv_pos_as_xyz
-  1  'x, y, z'
-loop_
- _atom_site_type_symbol
- _atom_site_label
- _atom_site_symmetry_multiplicity
- _atom_site_fract_x
- _atom_site_fract_y
- _atom_site_fract_z
- _atom_site_occupancy
-  Ac  Ac0  1  0.00000000  0.00000000  0.00000000  1
-  Ac  Ac1  1  0.33333333  0.66666667  0.25000000  1
-  Ac  Ac2  1  0.00000000  0.00000000  0.50000000  1
-  Ac  Ac3  1  0.66666667  0.33333333  0.75000000  1
-`;
-const mol = ChemDoodle.readCIF(cif);
-console.log(mol);
+// 加载CIF文件并创建模型
+const loader = new THREE.FileLoader();
+loader.load("/cif/Ac2CdGa.cif", function (cif) {
+  const mol = ChemDoodle.readCIF(cif, 0);
+  const model = new THREE.Group();
+  const molecule = mol.molecule;
+  const atoms = molecule.atoms;
+  const bonds = molecule.bonds;
+  const atomCount = atoms.length;
+  const bondCount = bonds.length;
+  const gltfLoader = new GLTFLoader();
 
-const model = new THREE.Group();
-const molecule = mol.molecule;
-const atoms = molecule.atoms;
-const atomCount = atoms.length;
-const gltfLoader = new GLTFLoader();
-gltfLoader.load(
-  "/ball/ball.gltf",
-  (gltf) => {
-    // gltf.scene.scale.set(30, 30, 30);
-    // const doorLight = new THREE.PointLight("#ffffff", 1, 7);
-    // doorLight.position.set(0, 60, -10);
-    // // helper
-    // const doorLightHelper = new THREE.PointLightHelper(doorLight, 1);
-    // doorLight.castShadow = true;
-    // doorLight.shadow.camera.near = 1;
-    // doorLight.shadow.camera.far = 20;
-    // doorLight.shadow.mapSize.set(1024, 1024);
-    // doorLight.shadow.radius = 5;
-    // doorLight.shadow.bias = -0.0001;
-    // doorLight.shadow.normalBias = 0.02;
-    // doorLight.shadow.camera.updateProjectionMatrix();
-    // gltf.scene.add(doorLight);
-    // scene.add(doorLightHelper);
+  // 添加光源以使模型可见
+  const light = new THREE.DirectionalLight(0xffffff, 1.5);
+  light.position.set(-10, 10, -10);
+  scene.add(light);
 
-    // gltf.scene.traverse((item) => {
-    //   if (item.name === "Cube") {
-    //     item.castShadow = true;
-    //     createSquare(5, 5, item, gltf.scene, 4.2);
-    //     item.visible = false;
-    //   }
-    // });
+  gltfLoader.load(
+    "/ball/ball.gltf",
+    (gltf) => {
+      // gltf.scene.scale.set(30, 30, 30);
+      // const doorLight = new THREE.PointLight("#ffffff", 1, 7);
+      // doorLight.position.set(0, 60, -10);
+      // // helper
+      // const doorLightHelper = new THREE.PointLightHelper(doorLight, 1);
+      // doorLight.castShadow = true;
+      // doorLight.shadow.camera.near = 1;
+      // doorLight.shadow.camera.far = 20;
+      // doorLight.shadow.mapSize.set(1024, 1024);
+      // doorLight.shadow.radius = 5;
+      // doorLight.shadow.bias = -0.0001;
+      // doorLight.shadow.normalBias = 0.02;
+      // doorLight.shadow.camera.updateProjectionMatrix();
+      // gltf.scene.add(doorLight);
+      // scene.add(doorLightHelper);
 
-    // gltf.scene.traverse((item) => {
-    //   if (item.name === "Box033_Chrome_0001") {
-    //     item.castShadow = true;
-    //     createLattice(5, item, gltf.scene);
-    //     item.visible = false;
-    //   }
-    // });
-    // const _scene2 = gltf.scene.clone();
-    // _scene2.name = "right_door";
-    // // _scene2.rotation.y = -Math.PI;
-    // _scene2.children[0].translateZ(1);
-    // _scene2.position.set(0, 0, -2);
-    // right_door = _scene2;
-    // gltf.scene.add(doorLight);
-    // scene.add(doorLightHelper);
-    let _ball = gltf.scene;
-    // scene.add(_ball);
+      // gltf.scene.traverse((item) => {
+      //   if (item.name === "Cube") {
+      //     item.castShadow = true;
+      //     createSquare(5, 5, item, gltf.scene, 4.2);
+      //     item.visible = false;
+      //   }
+      // });
 
-    for (let i = 0; i < atomCount; i++) {
-      // const ball = _ball!.clone();
-      // ball.name = "ball_" + i;
-      const atom = atoms[i];
-      const { x, y, z } = atom;
-      // ball.position.set(x, y, z);
-      // ball.scale.set(0.5, 0.5, 0.5);
-      const color = new THREE.Color(
-        Math.random(),
-        Math.random(),
-        Math.random()
-      );
-      // (ball as THREE.Mesh).material.color = color;
-      const _scene = _ball.clone();
-      _scene.position.set(x, y, z);
-      _scene.scale.set(0.2, 0.2, 0.2);
-      _scene.traverse((child) => {
-        if (child.isMesh && child.material) {
-          if (Array.isArray(child.material)) {
-            // 对于具有多重材质的对象
-            child.material = child.material.map((mat) => {
-              const newMat = mat.clone();
-              newMat.color.copy(color);
-              return newMat;
-            });
-          } else {
-            // 对于只有一个材质的对象
-            child.material = child.material.clone();
-            child.material.color.copy(color);
+      // gltf.scene.traverse((item) => {
+      //   if (item.name === "Box033_Chrome_0001") {
+      //     item.castShadow = true;
+      //     createLattice(5, item, gltf.scene);
+      //     item.visible = false;
+      //   }
+      // });
+      // const _scene2 = gltf.scene.clone();
+      // _scene2.name = "right_door";
+      // // _scene2.rotation.y = -Math.PI;
+      // _scene2.children[0].translateZ(1);
+      // _scene2.position.set(0, 0, -2);
+      // right_door = _scene2;
+      // gltf.scene.add(doorLight);
+      // scene.add(doorLightHelper);
+      let _ball = gltf.scene;
+      // scene.add(_ball);
+
+      for (let i = 0; i < atomCount; i++) {
+        // const ball = _ball!.clone();
+        // ball.name = "ball_" + i;
+        const atom = atoms[i];
+        const { x, y, z } = atom;
+        // ball.position.set(x, y, z);
+        // ball.scale.set(0.5, 0.5, 0.5);
+        const color = new THREE.Color(
+          Math.random(),
+          Math.random(),
+          Math.random()
+        );
+        // (ball as THREE.Mesh).material.color = color;
+        const _scene = _ball.clone();
+        _scene.position.set(x, y, z);
+        _scene.scale.set(0.3, 0.3, 0.3);
+        _scene.traverse((child) => {
+          if (child.isMesh && child.material) {
+            if (Array.isArray(child.material)) {
+              // 对于具有多重材质的对象
+              child.material = child.material.map((mat) => {
+                const newMat = mat.clone();
+                newMat.color.copy(color);
+                return newMat;
+              });
+            } else {
+              // 对于只有一个材质的对象
+              child.material = child.material.clone();
+              child.material.color.copy(color);
+            }
           }
-        }
-      });
-      model.add(_scene);
+        });
+        model.add(_scene);
+      }
+
+      for (let i = 0; i < bondCount; i++) {
+        const bond = bonds[i];
+        const { a1, a2 } = bond;
+        const height = a2.distance3D(a1);
+
+        const cylinderGeometry = new THREE.CylinderGeometry(
+          0.1,
+          0.1,
+          height,
+          8
+        );
+        const color = new THREE.Color(
+          Math.random(),
+          Math.random(),
+          Math.random()
+        );
+        const cylinderMaterial = new THREE.MeshPhongMaterial({
+          color,
+          shininess: 100,
+        });
+        var cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+        cylinderMesh.position.set(
+          (a1.x + a2.x) / 2,
+          (a1.y + a2.y) / 2,
+          (a1.z + a2.z) / 2
+        );
+        let direction = new THREE.Vector3();
+
+        direction.subVectors(a2, a1).normalize();
+
+        let axis = new THREE.Vector3(0, 1, 0);
+        //使用Quaternion对象创建旋转矩阵，并将其应用于氢键模型
+        let quaternion = new THREE.Quaternion();
+
+        quaternion.setFromUnitVectors(axis, direction);
+        cylinderMesh.setRotationFromQuaternion(quaternion);
+
+        model.add(cylinderMesh);
+      }
+
+      const box = new THREE.Box3().setFromObject(model);
+      const center = new THREE.Vector3();
+      const c = box.getCenter(center).negate();
+
+      model.position.setX(c.x);
+      model.position.setY(c.y);
+      model.position.setZ(c.z);
+      scene.add(model);
+    },
+    (progress) => {},
+    (error) => {
+      console.log("error");
+      console.log(error);
     }
-
-    model.position.setX(-3);
-    model.position.setY(-3);
-    model.position.setZ(-3);
-    scene.add(model);
-
-    // scene.add(gltf.scene);
-  },
-  (progress) => {},
-  (error) => {
-    console.log("error");
-    console.log(error);
-  }
-);
+  );
+});
 
 function animate() {
   requestAnimationFrame(animate);
